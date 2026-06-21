401212e65cbab556a777d0634e4ffa2ffb35975c
Geo Vega <george.h.vega1@gmail.com>
2026-06-20 22:55:42 -0700

    Fix Nunjucks filter syntax for date and limit

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,12 @@ module.exports = function(eleventyConfig) {
     return collectionApi.getFilteredByGlob("src/blog/posts/*.md").reverse();
   });
 
+  eleventyConfig.addFilter("dateDisplay", (date) => {
+    return new Date(date).toLocaleDateString("en-GB", { year: "numeric", month: "long", day: "numeric" });
+  });
+
+  eleventyConfig.addFilter("limit", (arr, n) => arr.slice(0, n));
+
   return {
     dir: {
       input: "src",
